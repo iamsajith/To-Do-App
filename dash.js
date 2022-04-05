21476ee4dc1a36dc527efe68de799a8edb954c25
iamsajith <sajithj03@gmail.com>
2022-04-05 12:03:39 +0530

    changes

--- dash.js
+++ dash.js
@@ -1,26 +1,36 @@
-var apiData=()=>{
- var xhttp = new XMLHttpRequest();
- xhttp.onreadystatechange = () => {
-if (xhttp.readyState === 4 && xhttp.status === 200){
- var response = JSON.parse(xhttp.responseText);
- var output = "";
- for(var i = 0; i <response.length; i++){
-  var fetch = response[i].title
-  output += `<div class="d-flex justify-content-start align-items-center p-3" id="todo-cont"><input type="checkbox" id="check" class="form-check-input mx-3" onclick="myFunction()">${fetch}
+var apiData = () => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      var response = JSON.parse(xhttp.responseText);
+      var output = "";
+      for (var i = 0; i < response.length; i++) {
+        var fetch = response[i].title;
+        output += `<div class="d-flex justify-content-start align-items-center p-3" id="todo-cont"><input type="checkbox" id="check" class="form-check-input m-3 p-2" onclick="myFunction();">${fetch}
 </div>`;
-  document.getElementById("todo-list").innerHTML = output;
- }
- 
-}
- }
- xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
- xhttp.send();
-}
-var check = document.getElementById('check');
-var count = 0
-var myFunction = ()=>{
-if (check.checked==true){
-count += 1
-console.log(count)
-}
-}
+      }
+      document.getElementById("todo-list").innerHTML = output;
+    }
+  };
+  xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+  xhttp.send();
+};
+var count = 0;
+var myFunction = () => {
+  let p = new Promise((resolve, reject) => {
+    var check = document.getElementById("check");
+    if (check.checked == true) {
+      count += 1;
+      if (count == 5) {
+        resolve("Success");
+      }
+      console.log(count);
+    } else {
+      count -= 1;
+      console.log(count);
+    }
+  });
+  p.then((message) => {
+    alert(message);
+  });
+};
--- index.js
+++ index.js
@@ -1,62 +1,57 @@
-const form = document.getElementById('smt');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-var count = 0
+const form = document.getElementById("smt");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+var count = 0;
 
-var checkInputs = (callback)=> {
-	const userValue = username.value.trim();
-	const passwordValue = password.value.trim();
+var checkInputs = (callback) => {
+  const userValue = username.value.trim();
+  const passwordValue = password.value.trim();
 
-
-if(userValue === '') { 
- console.log('no-blank')
- setErrorFor1('Username cannot be blank');
-} else if (userValue != 'admin') {
- console.log('not-valid')
- setErrorFor1('Username is not found');
-} else {
- setSuccessFor1();
- count = 0
- count += 1
-}
-if(passwordValue === '') {
- setErrorFor2('Password cannot be blank');
-}
- else if(passwordValue != "12345"){ 
-   setErrorFor2('Not a valid password');
- }
- else{
- setSuccessFor2();
- count += 1
- console.log(count)
+  if (userValue === "") {
+    console.log("no-blank");
+    setErrorFor1("Username cannot be blank");
+  } else if (userValue != "admin") {
+    console.log("not-valid");
+    setErrorFor1("Username is not found");
+  } else {
+    setSuccessFor1();
+    count = 0;
+    count += 1;
+  }
+  if (passwordValue === "") {
+    setErrorFor2("Password cannot be blank");
+  } else if (passwordValue != "12345") {
+    setErrorFor2("Not a valid password");
+  } else {
+    setSuccessFor2();
+    count += 1;
+    console.log(count);
+  }
+  var out = callback();
+  return out;
 };
-var out = callback();
-return out;
+var redirect = () => {
+  if (count == 2) {
+    alert("You are logged in");
+    return true;
+  } else {
+    return false;
+  }
+};
 
-}
-var redirect=()=>{
-	if (count == 2){
-		alert('You are logged in')
-		return true
-	}
-	else{
-		return false
-	}
-}
-
-var setErrorFor1 = (message)=>{
- const formControl = document.getElementsByTagName('small')[0];
-	formControl.classList.add('error');
-	formControl.innerHTML = message;
-}
-var setErrorFor2 = (message)=>{
- const formControl = document.getElementsByTagName('small')[1];
-	formControl.className ="error";
-	formControl.innerHTML = message;
-}
-var setSuccessFor1 = ()=>{
- document.getElementsByTagName('small')[0].className = "success"
-}
-var setSuccessFor2 = ()=>{
- document.getElementsByTagName('small')[1].className = "success"
-}
+var setErrorFor1 = (message) => {
+  const formControl = document.getElementsByTagName("small")[0];
+  formControl.classList.add("error");
+  formControl.innerHTML = message;
+};
+var setErrorFor2 = (message) => {
+  const formControl = document.getElementsByTagName("small")[1];
+  formControl.className = "error";
+  formControl.innerHTML = message;
+};
+var setSuccessFor1 = () => {
+  document.getElementsByTagName("small")[0].className = "success";
+};
+var setSuccessFor2 = () => {
+  document.getElementsByTagName("small")[1].className = "success";
+};
